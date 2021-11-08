af581b793369c55ef06c4fc926316d2766e6d058
Nikita <medvedhopod@gmail.com>
2021-11-08 14:07:40 +0200

    Hangman 2-st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,11 +1,17 @@
 package Hangman;
+import java.util.Objects;
 import java.util.Scanner;
 
 
 public class Hangman {
     public static void main(String[] args) {
-//        Scanner scanner = new
+        Scanner input = new Scanner(System.in);
         System.out.println("HANGMAN");
-
+        String user = input.nextLine();
+        if (Objects.equals(user, "java")){
+            System.out.println("You survived!");
+        } else{
+            System.out.println("You lost!");
+        }
     }
 }
