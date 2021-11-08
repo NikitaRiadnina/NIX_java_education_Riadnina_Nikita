7ccdbb7c5c830bc5dd9ed9b66ce183c1b85f3472
Nikita <medvedhopod@gmail.com>
2021-11-08 22:03:10 +0200

    Hangman 4-st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -7,6 +7,7 @@ import java.util.Random;
 public class Hangman {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        StringBuffer buffer = new StringBuffer("");
         System.out.println("HANGMAN");
         ArrayList<String> lst = new ArrayList<>();
         lst.add("python");
@@ -16,7 +17,11 @@ public class Hangman {
 
         String word = lst.get(new Random().nextInt(lst.size()));
         int repeat = 0;
+        buffer.append(word);
+        StringBuffer help = buffer.delete(2, buffer.length());
+        for (int i = 1; i <= word.length() - 2; i++) help.append("-");
         while (repeat != 8) {
+            System.out.println("Guess the word" + " " + help + ":");
             String user = input.nextLine();
             if (word.equals(user)) {
                 System.out.println("You survived!");
