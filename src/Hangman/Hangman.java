e3016f25943a8d33edb4a9df2b0ba056f615f1ef
Nikita <medvedhopod@gmail.com>
2021-11-08 13:52:32 +0200

    Hangman 1-st stage

---
+++ src/Hangman/Hangman.java
@@ -0,0 +1,11 @@
+package Hangman;
+import java.util.Scanner;
+
+
+public class Hangman {
+    public static void main(String[] args) {
+//        Scanner scanner = new
+        System.out.println("HANGMAN");
+
+    }
+}
