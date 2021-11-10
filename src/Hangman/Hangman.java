3249019d229b25b3efdc7db08d04c53504701170
Nikita <medvedhopod@gmail.com>
2021-11-10 14:10:36 +0200

    Hangman 6-st stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -10,6 +10,7 @@ public class Hangman {
         StringBuffer user_word = new StringBuffer("");
         System.out.println("HANGMAN");
         ArrayList<String> lst = new ArrayList<>();
+        ArrayList<String> storage = new ArrayList<>();
         lst.add("python");
         lst.add("java");
         lst.add("javascript");
@@ -19,11 +20,24 @@ public class Hangman {
         int heart = 8;
         for (int i = 1; i <= word.length(); i++) user_word.append("-");
         while (heart != 0) {
-            System.out.println(user_word);
+            System.out.println("\n" + user_word);
             System.out.println("Input a letter:");
             String user = input.nextLine();
             int index = word.indexOf(user);
-            if (index >= 0){
+//            System.out.println(check);
+
+            if (index < 0 || storage.contains(user)){
+                if (storage.contains(user)) {
+                    System.out.println("No improvements");
+                } else {
+                    System.out.println("Than letter doesn't appear in the word");
+                }
+                storage.add(user);
+                heart--;
+//
+            } else {
+//                boolean check_storage = storage.contains(user);
+                storage.add(user);
                 if (user.equals("a")){
                     user_word.insert(index, user);
                     user_word.delete(index + 1, index + 2);
@@ -33,11 +47,14 @@ public class Hangman {
                     user_word.insert(index, user);
                     user_word.delete(index + 1, index + 2);
                 }
-            } else {
-                System.out.println("Than letter doesn't appear in the word");
+                if (word.contains(user_word)) {
+                    System.out.println(user_word + "\nYou guessed the word!\nYou survived!");
+                    break;
+                }
             }
-            heart--;
         }
-        System.out.println("Thanks for playing!\nWe'll see how well you did in the next stage");
+        if (0 >= heart){
+            System.out.println("You lost!");
+        }
     }
 }
